1b8f6095bf92f9c02f63892a7a06887bf72edf27
mac <ndql1996@gmail.com>
2020-10-14 16:17:38 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -423,7 +423,7 @@
         = COUNTDOWN CLOCK
     -------------------------------------------*/
     if ($("#clock").length) {
-        $('#clock').countdown('2020/12/31', function(event) {
+        $('#clock').countdown('2020/11/22', function(event) {
             var $this = $(this).html(event.strftime(''
             + '<div class="box"><div>%D</div> <span>Days</span> </div>'
             + '<div class="box"><div>%H</div> <span>Hours</span> </div>'
